5ffe2d0287b1ef4f8e37ca9fc86aba71e8332414
Luka John <lukajohn70@users.noreply.github.com>
2026-08-24 20:04:10 +0100

    fix: add 'options' array cast to Question model to fix JSON serialization error

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -18,6 +18,10 @@ class Question extends Model
         'order',
     ];
 
+    protected $casts = [
+        'options' => 'array',
+    ];
+
     public function quiz()
     {
         return $this->belongsTo(Quiz::class);
